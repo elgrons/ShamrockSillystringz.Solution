89e0614cd7a742956a55146d480382e60368fea9
Eliot Gronstal <eliot.lauren@gmail.com>
2023-03-17 09:23:17 -0700

    update Views.Home.Index and Models for initial database buildout and to confirm functionality.

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System;
 
@@ -5,6 +6,11 @@ namespace Factory.Models
 {
   public class Engineer
   {
-
+    public int EngineerId { get; set; }
+    [Required(ErrorMessage = "The engineer's name can't be empty.")]
+    public string EngineerName { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "You must add an engineer to a machine. Has a machine been added yet?")]
+    public string EngineerDetails { get; set; }
+    public List<EngineerMachine> JoinEntities { get; }
   }
 }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System;
 
@@ -5,6 +6,11 @@ namespace Factory.Models
 {
   public class Machine
   {
-
+    public int MachineId { get; set; }
+    [Required(ErrorMessage = "The machine name field can't be empty!")]
+    public string MachineName { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "You must add a machine to an engineer. Has an engineer been added yet?")]
+    public DateTime ServiceDate { get; set; }
+    public List<EngineerMachine> JoinEntities { get; }
   }
 }
