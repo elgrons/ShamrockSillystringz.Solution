ec59eaa001ada7bccce84ce614002f1e4bc2ac0c
Eliot Gronstal <eliot.lauren@gmail.com>
2023-03-17 14:08:23 -0700

    update Controllers.Engineers and have functionality again.

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using Factory.Models;
-using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -9,26 +8,26 @@ namespace Factory.Controllers
 {
   public class EngineersController : Controller
   {
-      private readonly FactoryContext _db;
+    private readonly FactoryContext _db;
+
     public EngineersController(FactoryContext db)
     {
       _db = db;
     }
-
     public ActionResult Index()
     {
       return View(_db.Engineers.ToList());
     }
+
     public ActionResult Details(int id)
     {
       ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "MachineName");
       Engineer thisEngineer = _db.Engineers
           .Include(engineer => engineer.JoinEntities)
           .ThenInclude(join => join.Machine)
-          .FirstOrDefault(engineer => engineer.EngineerId == id);
+          .FirstOrDefault(engieer => engieer.EngineerId == id);
       return View(thisEngineer);
     }
-
     public ActionResult Create()
     {
       return View();
@@ -37,77 +36,68 @@ namespace Factory.Controllers
     [HttpPost]
     public ActionResult Create(Engineer engineer)
     {
-      if (!ModelState.IsValid)
+      _db.Engineers.Add(engineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddMachine(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineers => engineers.EngineerId == id);
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "MachineName");
+      return View(thisEngineer);
+    }
+
+    [HttpPost]
+    public ActionResult AddMachine(Engineer engineer, int machineId)
+    {
+      #nullable enable
+      EngineerMachine? joinEntity = _db.EngineerMachines.FirstOrDefault(join => (join.MachineId == machineId && join.EngineerId == engineer.EngineerId));
+      #nullable disable
+      if (joinEntity == null && machineId != 0)
       {
-          ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "EngineerName");
-          return View(engineer);
-      }
-      else
-      {
-        _db.Engineers.Add(engineer);
+        _db.EngineerMachines.Add(new EngineerMachine() { MachineId = machineId, EngineerId = engineer.EngineerId });
         _db.SaveChanges();
-        return RedirectToAction("Index");
       }
+      return RedirectToAction("Details", new { id = engineer.EngineerId });
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineers => engineers.EngineerId == id);
+      return View(thisEngineer);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Engineer engineer)
+    {
+      _db.Engineers.Update(engineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineers => engineers.EngineerId == id);
+      return View(thisEngineer);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineers => engineers.EngineerId == id);
+      _db.Engineers.Remove(thisEngineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteJoin(int joinId)
+    {
+      EngineerMachine joinEntry = _db.EngineerMachines.FirstOrDefault(entry => entry.EngineerMachineId == joinId);
+      _db.EngineerMachines.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
-//     public ActionResult AddMachine(int id)
-//     {
-//       Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineers => engineers.EngineerId == id);
-//       ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "MachineName");
-//       return View(thisEngineer);
-//     }
-
-//     [HttpPost]
-//     public ActionResult AddMachine(Engineer engineer, int machineId)
-//     {
-//       #nullable enable
-//       EngineerMachine? joinEntity = _db.EngineerMachines.FirstOrDefault(join => (join.MachineId == machineId && join.EngineerId == engineer.EngineerId));
-//       #nullable disable
-//       if (joinEntity == null && machineId != 0)
-//       {
-//         _db.EngineerMachines.Add(new EngineerMachine() { MachineId = machineId, EngineerId = engineer.EngineerId });
-//         _db.SaveChanges();
-//       }
-//       return RedirectToAction("Details", new { id = engineer.EngineerId });
-//     }
-
-//     public ActionResult Edit(int id)
-//     {
-//       Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineers => engineers.EngineerId == id);
-//       return View(thisEngineer);
-//     }
-
-//     [HttpPost]
-//     public ActionResult Edit(Engineer engineer)
-//     {
-//       _db.Engineers.Update(engineer);
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
-
-//     public ActionResult Delete(int id)
-//     {
-//       Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineers => engineers.EngineerId == id);
-//       return View(thisEngineer);
-//     }
-
-//     [HttpPost, ActionName("Delete")]
-//     public ActionResult DeleteConfirmed(int id)
-//     {
-//       Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineers => engineers.EngineerId == id);
-//       _db.Engineers.Remove(thisEngineer);
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
-
-//     [HttpPost]
-//     public ActionResult DeleteJoin(int joinId)
-//     {
-//       EngineerMachine joinEntry = _db.EngineerMachines.FirstOrDefault(entry => entry.EngineerMachineId == joinId);
-//       _db.EngineerMachines.Remove(joinEntry);
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
-//   }
-// }
